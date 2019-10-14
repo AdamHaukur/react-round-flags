dbbfd154ceb14f1614a69567f8b9002b7374de3c
Sisir K. Adhikari <sisir@tutremix.com>
2019-10-14 15:45:31 +0600

    Component update

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-// @flow
 
 import React from 'react';
-import { Image } from 'react-native';
+import PropTypes from 'prop-types';
 import * as flags from './flags';
 
-type Props = {
-  code: string,
-  style?: any,
-};
-
-const Flag = ({ size = 64, code, type = 'shiny', style }: Props) => {
+const Flag = ({ size = 64, code, type = 'shiny', style }) => {
   const flag = flags[`icons${size}`][code];
   const unknownFlag = flags[`icons${size}`]['unknown'];
 
-  return (
-    <Image
-      source={flag || unknownFlag}
+  return <img
+      src={flag || unknownFlag}
       style={[{ width: size, height: size }, style]}
     />
-  );
 };
+
+Flag.propTypes = {
+  code: PropTypes.string.isRequired,
+  style: PropTypes.any
+}
 
 export default Flag;
